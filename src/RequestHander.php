cbcb7184943484ae15df31f887ef5d95f1defd68
projectair <airdesign.code@gmail.com>
2019-09-30 14:58:13 +0100

    requesthandler class added

---
+++ src/RequestHander.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Spatial\Psr7;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class RequestHandler implements RequestHandlerInterface
+{
+    function __construct()
+    {
+        $this->response = new Response();
+    }
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $payload = json_encode(['success' => true, 'message' => 'Psr7 Works!', 'data' => ['value1', 'value2']]);
+        $this->response->getBody()->write($payload);
+        return $this->response;
+    }
+}
