b72d7cf07c0f5df6b5fd6d9bae8440b1044edab0
Kofi Owusu-Afriyie <airdesign.code@gmail.com>
2019-10-21 11:24:06 +0100

    privitized response

--- src/RequestHander.php
+++ src/RequestHander.php
@@ -8,13 +8,21 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 class RequestHandler implements RequestHandlerInterface
 {
+    private $response;
+    
     function __construct()
     {
         $this->response = new Response();
     }
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $payload = json_encode(['success' => true, 'message' => 'Psr7 Works!', 'data' => ['value1', 'value2']]);
+        $payload = json_encode(
+            [
+                'success' => true,
+                'message' => 'Psr7 Works!',
+                'data' => ['value1', 'value2']
+            ]
+        );
         $this->response->getBody()->write($payload);
         return $this->response;
     }
